ec4dc126535841b95f232b02222eae1ba55e0f52
Zelefack123 <133670896+Zelefack123@users.noreply.github.com>
2023-05-30 14:53:21 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-ReactDOM.render(<APP/> , document.getElementById('root'));
+import ReactDOM from "react-dom";
+import App from './App'
+import './index.css';
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
+
+//the above line indicates that all the content of this file should be rendered in the id root of the html file
+
+
